e0e62fec55a816bf96537e064dbe849ee00b77f6
matiaswasiak <matiaswasiak@gmail.com>
2022-03-22 02:36:04 -0300

    Layout for Error404.js and adding a font

--- client/src/components/pages/Error404.js
+++ client/src/components/pages/Error404.js
@@ -5,8 +5,10 @@ const Error404 = () => {
     <Container>
       <Card>
         <CardInfo>
-          <img src="/images/404.png" alt="404 Snorlax" />
-          <p>Oops! A wild Snorlax has blocked your path!</p>
+          <img src="/images/404.svg" alt="404 Snorlax" />
+          <p>
+            <span>Oops!</span> A wild Snorlax has blocked your path!
+          </p>
           <button>Go Back</button>
         </CardInfo>
       </Card>
@@ -22,19 +24,53 @@ const Container = styled.main`
 `;
 
 const Card = styled.div`
-  width: 60vw;
-  height: 60vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  width: 1200px;
+  height: 600px;
   margin: auto;
   border-radius: 20px;
   box-shadow: 0px 4px 55px rgba(0, 0, 0, 0.25);
   background: linear-gradient(#244049, #20353c);
-  background-color: red;
 `;
 
 const CardInfo = styled.div`
-  width: 60%;
-  height: 100%;
+  position: absolute;
+  display: inherit;
+  flex-direction: column;
   justify-content: center;
+  text-align: center;
+  width: 60%;
+  color: #fbffff;
+  font-family: "Lato";
+
+  img {
+    height: 360px;
+  }
+
+  p {
+    font-size: 32px;
+    line-height: 42px;
+    margin: 10px 0 36px 0;
+
+    span {
+      color: #ecc92a;
+    }
+  }
+
+  button {
+    margin: 0 auto;
+    width: 40%;
+    height: 75px;
+    background-color: #ffcb04;
+    border-radius: 110px;
+    border: none;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 30px;
+    cursor: pointer;
+  }
 `;
 
 export default Error404;
