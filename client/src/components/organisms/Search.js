e13497c6e876d20374bc066d7aa27aadb8842d3e
matiaswasiak <matiaswasiak@gmail.com>
2022-03-22 03:54:48 -0300

    Layout for Search.js

--- client/src/components/organisms/Search.js
+++ client/src/components/organisms/Search.js
@@ -1,7 +1,40 @@
-import React from "react";
+import styled from "styled-components";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <SearchBar>
+      <input type="text" placeholder="Search for pokemons..." />
+      <input type="submit" />
+    </SearchBar>
+  );
 };
+
+const SearchBar = styled.form`
+  display: flex;
+  height: 50px;
+
+  input:first-child {
+    position: relative;
+    width: 700px;
+    height: 100%;
+    border: 2px solid #d5a8f9;
+    box-sizing: border-box;
+    border-radius: 20px;
+    background: #f0edf2;
+    padding: 0 20px;
+    font-size: 16px;
+  }
+
+  input:last-child {
+    margin-left: -20px;
+    width: 120px;
+    height: 50px;
+    background: #d5a8f9;
+    border: 2px solid #d5a8f9;
+    border-radius: 0px 20px 20px 0px;
+    color: #ffffff;
+    font-size: 18px;
+  }
+`;
 
 export default Search;
