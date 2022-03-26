ffab4f365d48555a4fac4741e783d53efb91cebf
matiaswasiak <matiaswasiak@gmail.com>
2022-03-26 04:15:22 -0300

    detailPokemon.js

--- client/src/components/pages/DetailPokemon.js
+++ client/src/components/pages/DetailPokemon.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
-import { getDetails, getPokemons } from "../../redux/actions";
+import { getDetails } from "../../redux/actions";
 import Footer from "../sections/Footer";
 import Header from "../sections/Header";
 
